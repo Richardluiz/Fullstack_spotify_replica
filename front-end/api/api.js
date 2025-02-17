0efacb72137c20876c4682c1a4d2aadd0a23c0a7
Richardluiz <richardluizborba@hotmail.com>
2025-02-17 16:03:34 -0300

    finalizando o projeto

---
+++ front-end/api/api.js
@@ -0,0 +1,15 @@
+// Fetch ou Axios
+import axios from "axios";
+import "dotenv/config";
+
+const { NODE_ENV } = process.env;
+const URL = NODE_ENV === "development" ? "http://localhost:3001/api" : "/api";
+
+
+const responseArtists = await axios.get(`${URL}/artists`);
+const responseSongs = await axios.get(`${URL}/songs`);
+
+export const artistArray = responseArtists.data;
+export const songsArray = responseSongs.data;
+
+// console.log(responseArtists.data);
