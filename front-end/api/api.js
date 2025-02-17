06d1d7c3c4f55f3f25f6a1eccff437603d85f1a0
Richardluiz <richardluizborba@hotmail.com>
2025-02-17 17:47:44 -0300

    update to  deploy

--- front-end/api/api.js
+++ front-end/api/api.js
@@ -2,8 +2,7 @@
 import axios from "axios";
 
 
-const { NODE_ENV } = process.env;
-const URL = NODE_ENV === "development" ? "http://localhost:3001/api" : "/api";
+const URL = "https://fullstack-spotify-replica.onrender.com/api";
 
 
 const responseArtists = await axios.get(`${URL}/artists`);
